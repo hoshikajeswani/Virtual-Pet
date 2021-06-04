2bd805d11875003a50c1a699ac4add552534edd1
hoshikajeswani <76256631+hoshikajeswani@users.noreply.github.com>
2021-06-04 14:53:14 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,96 @@
+var dog,sadDog,happyDog, database;
+var foodS,foodStock;
+var addFood;
+var foodObj;
+//create feed and lastFed variable here
+var feed,lastFed;
+var fedTime;
+
+
+function preload(){
+sadDog=loadImage("Dog.png");
+happyDog=loadImage("happy dog.png");
+}
+
+function setup() {
+  database=firebase.database();
+  createCanvas(1000,400);
+
+  foodObj = new Food();
+
+  foodStock=database.ref('Food');
+  foodStock.on("value",readStock);
+  
+  dog=createSprite(800,200,150,150);
+  dog.addImage(sadDog);
+  dog.scale=0.15;
+
+  //create feed the dog button here
+  feedFood=createButton("Feed The Dog")
+  feedFood.position(600,95)
+  feedFood.mousePressed(feedDog);
+
+  addFood=createButton("Add Food");
+  addFood.position(800,95);
+  addFood.mousePressed(addFoods);
+
+}
+
+function draw() {
+  background(46,139,87);
+  foodObj.display();
+
+  //write code to read fedtime value from the database 
+fedTime=database.ref("FeedTime")
+fedTime.on("value",function(data){
+  lastFed=data.val()
+})
+  
+ 
+  //write code to display text lastFed time here
+if(lastFed>=12){
+  fill("white")
+  text("Last Feed : "+lastFed%12+" PM",250,30);
+}else if(lastFed===0){
+  fill("white")
+text("Last Feed : 12 AM",250,30);
+}else{
+  fill("white")
+  text("Last Feed :"+lastFed+" AM",250,30)
+}
+ 
+  drawSprites();
+}
+
+//function to read food Stock
+function readStock(data){
+  foodS=data.val();
+  foodObj.updateFoodStock(foodS);
+}
+
+
+function feedDog(){
+  dog.addImage(happyDog);
+  //write code here to update food stock and last fed time
+  var foodvalue = foodObj.getFoodStock();
+  if(foodvalue <=0 ){
+    foodObj.updateFoodStock(0);
+  }else{
+    foodObj.updateFoodStock(foodvalue -1)
+  }
+
+  database.ref("/").update({
+    Food:foodObj.getFoodStock(),
+    FeedTime:hour()
+
+  })
+
+}
+
+//function to add food in stock
+function addFoods(){
+  foodS++;
+  database.ref('/').update({
+    Food:foodS
+  })
+}
